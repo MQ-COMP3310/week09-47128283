d5506de22391d90b07521d9c8988bdabf06bedc5
Ivan Machamer <ivan.machamer@students.mq.edu.au>
2025-05-08 10:46:13 +1000

    did  code scanning

--- project/auth.py
+++ project/auth.py
@@ -16,7 +16,8 @@ def login_post():
     password = request.form.get('password')
     remember = True if request.form.get('remember') else False
 
-    user = User.query.filter_by(email=email).first()
+    signup_query = text("select * from where email = user_email");
+    user = db.session.execute(signup_query, {"user_email":email}).all()
 
     # check if the user actually exists
     # take the user-supplied password and compare it with the stored password
